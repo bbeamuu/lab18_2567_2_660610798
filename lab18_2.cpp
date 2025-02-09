29b1fec629ba003810cbe991075bbc0e7cefa5b5
Beam <b3bb0z@gmail.com>
2025-02-09 23:18:31 +0700

    fn

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect* r1, Rect* r2) {
+    
+    double x_overlap = max(0.0, min(r1->x + r1->w, r2->x + r2->w) - max(r1->x, r2->x));
+    
+    double y_overlap = max(0.0, min(r1->y + r1->h, r2->y + r2->h) - max(r1->y, r2->y));
+    
+    return x_overlap * y_overlap;
 }
